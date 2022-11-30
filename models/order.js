f21af291a842a0104f83552aecc857ae7439caf0
Smit-Dama <smitdama09@gmail.com>
2022-11-30 17:47:53 +0530

    Added post hook to send order mail

--- models/order.js
+++ models/order.js
@@ -1,5 +1,8 @@
 const { Timestamp } = require('mongodb')
 const mongoose=require('mongoose')
+const dotenv = require('dotenv');
+const transporter =  require('../config/mail');
+const User = require('./user');
 const orderSchema =  new mongoose.Schema({
     user:
     {
@@ -27,3 +30,25 @@ const orderSchema =  new mongoose.Schema({
     }
 },{Timestamps:true}
 )
+orderSchema.post('save' , async function(doc , next){
+
+    try {
+        const user = User.findById(doc.user);
+        const info = await transporter.sendMail({
+            from:process.env.EMAIL_FROM,
+            to:user.email,
+            subject:'Signup Success',
+            text:'Welcome to our store , you have successfully ccreated a new account'
+    
+        });
+        console.log(info);
+        next();
+        
+    } catch (error) {
+        
+    }
+
+   
+
+
+})
